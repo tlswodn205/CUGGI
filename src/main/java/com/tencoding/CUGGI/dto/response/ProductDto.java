75f2ce3d98d4078fbabdc7c86e796eb96e51a65c
yndgow <yndgow@gmail.com>
2023-10-11 14:13:36 +0900

    2023 10 11 추가분
    섬네일 상세정보

--- src/main/java/com/tencoding/CUGGI/dto/response/ProductDto.java
+++ src/main/java/com/tencoding/CUGGI/dto/response/ProductDto.java
@@ -1,6 +1,7 @@
 package com.tencoding.CUGGI.dto.response;
 
 import java.sql.Timestamp;
+import java.text.DecimalFormat;
 
 import lombok.Data;
 
@@ -10,13 +11,16 @@ public class ProductDto {
 	private Integer id;
 	private Integer secondCategoryId;
 	private String productName;
-	private Integer price;
+	private String price;
+	private String productFeature;
+	private int quantity;
 	private Timestamp createdAt;
 	private String image;
 	private String secondCategoryName;
-	private boolean isThimbnail;
-	private int quantity;
-	private String productFeature;
-	private Integer categoryId;
 	
+	
+	public void setPrice(Integer price) {
+		DecimalFormat decimalFormat = new DecimalFormat("#,###");
+		this.price = decimalFormat.format(price);
+	}
 }
--- src/main/java/com/tencoding/CUGGI/service/ProductService.java
+++ src/main/java/com/tencoding/CUGGI/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.tencoding.CUGGI.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,25 +8,39 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tencoding.CUGGI.dto.response.ProductDto;
 import com.tencoding.CUGGI.dto.response.ProductListDto;
 import com.tencoding.CUGGI.repository.interfaces.ProductRepository;
 
 @Service
 public class ProductService {
 	
+
 	@Autowired
 	ProductRepository productRepository;
 	
+	/**
+	 * 상품 목록 
+	 * @param secondCategoryId
+	 * @param filter
+	 * @return 상품아이디별로 묶인 map
+	 */
 	public Map<Integer , List<ProductListDto>> productList(Integer secondCategoryId, String filter) {
 		
 		List<ProductListDto> productList = productRepository.findByAllForCateOrderByDesc(secondCategoryId, filter); // sql 결과
 		
-		Map<Integer , List<ProductListDto>> resultMap = new LinkedHashMap<>(); // 맵 생성
+		// 맵 변환, 상품아이디 별로 이미지를 묶어야함
+		Map<Integer , List<ProductListDto>> resultMap = new LinkedHashMap<>(); // . LinkedHashMap 맵을 만드는 순서대로 저장해주려면
 		for(ProductListDto dto : productList) {
 			Integer productId = dto.getProductId();
-			resultMap.computeIfAbsent(productId, ArrayList::new).add(dto);
+			resultMap.computeIfAbsent(productId, ArrayList::new).add(dto); // productId를 기준으로 없으면 추가
 		}
-		
 		return resultMap;
+	}
+	
+	
+	public List<ProductDto> productDetail(int productId) {
+		List<ProductDto> productDto = productRepository.findByIdForCate(productId); // sql
+		return productDto;
 	}
 }
