6899e0b523148ca1bcd9e8191bab218c9d0d17eb
qlr222 <40711583+qlr222@users.noreply.github.com>
2023-10-18 12:07:20 +0900

    헤더수정 (#48)

---
+++ src/main/java/com/tencoding/CUGGI/dto/response/CategoryResponseDto.java
@@ -0,0 +1,12 @@
+package com.tencoding.CUGGI.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CategoryResponseDto {
+	int id;
+	String secondCategoryName;
+	int firstCategoryId;
+	String firstCategoryName;
+	String image;
+}
