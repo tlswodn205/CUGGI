a72e307bccd4044c453132837a9ef8e4db255c76
qlr222 <qlr222@naver.com>
2023-11-24 18:09:29 +0900

    관리자 유저정보리스트 수정

--- src/main/java/com/tencoding/CUGGI/service/AdminService.java
+++ src/main/java/com/tencoding/CUGGI/service/AdminService.java
@@ -463,7 +463,6 @@ public class AdminService {
 		if(page <= 0) {
 			page = 1;
 		}
-
 		PagingResponseDto PagingResponseDto = userRepository.findPaging(type, kerword, page, status);
 		int startNum = (page-1)*10;
 		List<UserInfoListDto> userList = userRepository.findByKeywordAndCurrentPage(type, kerword, startNum, status);
